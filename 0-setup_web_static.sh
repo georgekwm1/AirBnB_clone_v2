91bb9efc73ff00a94499aaf1edaf1e21a342b446
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-05-07 14:55:20 -0700

    My Commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#Write a Bash script that sets up your web servers for the deployment of web_static
 
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
