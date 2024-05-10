611f9c4b8a17ae0e73fefc8a5e01b7929048073a
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-05-10 04:01:56 -0700

    My Commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """
-a Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import *
@@ -8,20 +7,7 @@ from datetime import datetime
 
 env.hosts = ['34.201.161.6', '100.25.12.248']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
-
-def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder
-    of your AirBnB Clone repo"""
-    local("mkdir -p versions")
-    date = datetime.now()
-    file_name = "versions/web_static_{}.tgz"\
-                .format(date.strftime("%Y%m%d%H%M%S"))
-    result = local("tar -cvzf {} web_static".format(file_name))
-    if result.failed:
-        return None
-    return file_name
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
@@ -34,5 +20,4 @@ def do_deploy(archive_path):
         return False
     result = run("mkdir -p /data/web_static/releases/{}/"\
              .format(folder_name))
-    if result.failed:
-        return False
+
