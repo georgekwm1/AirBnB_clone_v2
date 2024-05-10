509bfe576499b5b34ce83a41dea9e315b508a337
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-05-10 05:01:03 -0700

    My Commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import *
 from datetime import datetime
-
+from os.path import exists, isdir
 env.hosts = ['34.201.161.6', '100.25.12.248']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
@@ -14,25 +14,30 @@ env.key_filename = '~/.ssh/id_rsa'
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder
     of your AirBnB Clone repo"""
-    local("mkdir -p versions")
-    date = datetime.now()
-    file_name = "versions/web_static_{}.tgz"\
-                .format(date.strftime("%Y%m%d%H%M%S"))
-    result = local("tar -cvzf {} web_static".format(file_name))
-    if result.failed:
+    try:
+        date = datetime.now()
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz"\
+                    .format(date.strftime("%Y%m%d%H%M%S"))
+        result = local("tar -cvzf {} web_static".format(file_name))
+        result
+        return file_name
+    except:
         return None
-    return file_name
+            
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
-    file_name = archive_path.split("/")[-1]
-    folder_name = file_name.split(".")[0]
-    result = put(archive_path, "/tmp/")
-    if result.failed:
-        return False
-    result = run("mkdir -p /data/web_static/releases/{}/"\
-             .format(folder_name))
-    if result.failed:
+    try:
+        file_name = archive_path.split("/")[-1]
+        folder_name = file_name.split(".")[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/"\
+        .format(folder_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"\
+        .format(file_name, folder_name))
+    except:
         return False
